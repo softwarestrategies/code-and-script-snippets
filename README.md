e81ce42dba4935b4dac30194e23fdcb6587630db
henrydall@softwarestrategies.io <henrydall@softwarestrategies.io>
2022-04-21 10:01:01 -0700

    Cleaned up the code and added better docs

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Java Code Snippets
 
-## StringHasAllOfTheLettersInTheAlphabet
+## About
 
-Tests a string to see if it contains all 26 letters in the alphabet
+This is a WiP repository.  It has small applications & tests that either came from programming interview challenges or I have done just for fun.
 
+## Snippets
+
+### StringHasAllOfTheLettersInTheAlphabet
+
+Test whether a string to see if it contains all 26 letters in the alphabet
+
+### StringIsPalindrome
+
+Test whether a string is a palindrome or not
--- src/main/java/io/softwarestrategies/snippets/StringHasAllOfTheLettersInTheAlphabet.java
+++ src/main/java/io/softwarestrategies/snippets/StringHasAllOfTheLettersInTheAlphabet.java
@@ -6,15 +6,15 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * Tests a string to see if it contains all 26 letters in the alphabet
- * <p>
+ * Test whether a string to see if it contains all 26 letters in the alphabet
+ *
  * PROBLEM (interview question at DTS):
- * <p>
+ *
  * Given a long list of sentences (containing letters from the English alphabet and spaces) determine for each sentence
  * in the list whether it contains all the letters in the alphabet or not.
- * <p>
+ *
  * EXPLANATION:
- * <p>
+ *
  * Allowed the fact that a Set disallows duplicate entries to do the heavy-lifting.  This parses through the characters
  * of a sentence entered in at the command-line.  If the character is a letter (A-Z), add it to the set.  After all the
  * characters have been processed, if one or more instances of all the characters in the alphabet have occurred, the
@@ -28,19 +28,18 @@ public class StringHasAllOfTheLettersInTheAlphabet {
             "Just a final sentence to test"
     );
 
-
     public static void main(String[] args) {
         StringHasAllOfTheLettersInTheAlphabet app = new StringHasAllOfTheLettersInTheAlphabet();
 
         // Test True condition
         boolean result = app.sentenceHasAllLetters("There should be a fix way to visit the Santa Cruz zoo just in time for quick viewing of the baboons and koalas and pandas");
-        if (result == false) {
+        if (result != true) {
             throw new RuntimeException("Result should have been true");
         }
 
         // Test False condition
         result = app.sentenceHasAllLetters("Here is a dummy sentence.  It is very long and definitely doesn't have the right stuff and thus can be used to test failure even when long enough");
-        if (result == true) {
+        if (result != false) {
             throw new RuntimeException("Result should have been false");
         }
     }
